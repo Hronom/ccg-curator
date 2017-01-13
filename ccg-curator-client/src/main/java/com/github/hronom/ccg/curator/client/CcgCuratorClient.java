7c61c63a1040b99296cadfebb063328e14fec6ff
Hronom <hronom@gmail.com>
2017-01-14 01:04:17 +0200

    Start forming protocol

--- ccg-curator-client/src/main/java/com/github/hronom/ccg/curator/client/CcgCuratorClient.java
+++ ccg-curator-client/src/main/java/com/github/hronom/ccg/curator/client/CcgCuratorClient.java
@@ -1,26 +1,27 @@
 package com.github.hronom.ccg.curator.client;
 
-import com.github.hronom.ccg.curator.GreeterGrpc;
-import com.github.hronom.ccg.curator.HelloReply;
-import com.github.hronom.ccg.curator.HelloRequest;
+import com.github.hronom.ccg.curator.CardRevealedReply;
+import com.github.hronom.ccg.curator.CcgCuratorGrpc;
+import com.github.hronom.ccg.curator.SubscribeOnCardsShowdownRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 
-public class HelloWorldClient {
+public class CcgCuratorClient {
     private static final Logger logger = LogManager.getLogger();
 
     private final ManagedChannel channel;
-    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+    private final CcgCuratorGrpc.CcgCuratorBlockingStub blockingStub;
 
     /** Construct client connecting to HelloWorld server at {@code host:port}. */
-    public HelloWorldClient(String host, int port) {
+    public CcgCuratorClient(String host, int port) {
         this(ManagedChannelBuilder.forAddress(host, port)
             // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
             // needing certificates.
@@ -28,9 +29,9 @@ public class HelloWorldClient {
     }
 
     /** Construct client for accessing RouteGuide server using the existing channel. */
-    HelloWorldClient(ManagedChannelBuilder<?> channelBuilder) {
+    CcgCuratorClient(ManagedChannelBuilder<?> channelBuilder) {
         channel = channelBuilder.build();
-        blockingStub = GreeterGrpc.newBlockingStub(channel);
+        blockingStub = CcgCuratorGrpc.newBlockingStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
@@ -40,15 +41,12 @@ public class HelloWorldClient {
     /** Say hello to server. */
     public void greet(String name) {
         logger.info("Will try to greet " + name + " ...");
-        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-        HelloReply response;
-        try {
-            response = blockingStub.sayHello(request);
-        } catch (StatusRuntimeException e) {
-            logger.warn("RPC failed: {0}", e.getStatus());
-            return;
+        SubscribeOnCardsShowdownRequest request = SubscribeOnCardsShowdownRequest.newBuilder().setPlayerName(name).build();
+        Iterator<CardRevealedReply> response = blockingStub.subscribeOnCardsShowdown(request);
+        while (response.hasNext()) {
+            CardRevealedReply reply = response.next();
+            logger.info("Greeting: " + reply.getPlayerName() + " " + reply.getCardName());
         }
-        logger.info("Greeting: " + response.getMessage());
     }
 
     /**
@@ -56,7 +54,7 @@ public class HelloWorldClient {
      * greeting.
      */
     public static void main(String[] args) throws Exception {
-        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        CcgCuratorClient client = new CcgCuratorClient("localhost", 50051);
         try {
       /* Access a service running on the local machine on port 50051 */
             String user = "world";
